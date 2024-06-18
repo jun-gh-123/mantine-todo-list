d32058ed3fa160be3a75c53910a2e10a64e02068
a <a@a.com>
2024-06-18 12:07:56 -0400

    randomize placeholder for new task

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -2,9 +2,29 @@ import React, { useState } from "react";
 
 import { Title, TextInput, Group, Button } from "@mantine/core";
 
+function getRandomIntInclusive(min: number, max: number) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
+
+const randomTasks = [
+  "Buy a car",
+  "Go shopping",
+  "Run 30 minutes",
+  "Call an old friend",
+  "Laundry",
+  "Take a nap",
+  "Clean room",
+  "Do the dishes",
+];
+
 export default (props: { addNewTask: (value: string) => void }) => {
   const { addNewTask } = props;
   const [text, setText] = useState("");
+  const [phId, setPhId] = useState(
+    getRandomIntInclusive(0, randomTasks.length - 1)
+  );
 
   const onAdd = () => {
     if (!text) {
@@ -12,13 +32,14 @@ export default (props: { addNewTask: (value: string) => void }) => {
     }
     addNewTask(text);
     setText("");
+    setPhId(getRandomIntInclusive(0, randomTasks.length - 1));
   };
 
   return (
     <Group>
       <Title order={4}>Create New Task</Title>
       <TextInput
-        placeholder="Build a house"
+        placeholder={randomTasks[phId]}
         value={text}
         onChange={(e) => setText(e.currentTarget.value)}
         onKeyDown={(e) => {
